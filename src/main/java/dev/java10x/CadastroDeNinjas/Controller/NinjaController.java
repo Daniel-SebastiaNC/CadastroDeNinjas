e21290262390e6bf4446c13f47f78921c79f2e5c
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-07 11:14:26 -0300

    Entidade NinjaModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller/NinjaController.java
@@ -1,10 +1,12 @@
 package dev.java10x.CadastroDeNinjas.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+@RequestMapping
+public class NinjaController {
 
     @GetMapping("/hello")
     public String boasVindas(){
