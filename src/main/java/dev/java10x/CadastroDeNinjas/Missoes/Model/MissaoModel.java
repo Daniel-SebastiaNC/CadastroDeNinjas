44fcad0c5d6f840b371b2d78927d8a919bd2af4f
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-07 16:47:58 -0300

    feat: Coloca o Lombok na classe para abastracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Model/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Model/MissaoModel.java
@@ -2,44 +2,29 @@ package dev.java10x.CadastroDeNinjas.Missoes.Model;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.Model.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissaoModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
+    
     private RankMissao dificuldade;
 
     //Uma missão pode ter vários ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    /*public MissaoModel() {
-    }
-
-    public MissaoModel(String nome, RankMissao dificuldade) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public RankMissao getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(RankMissao dificuldade) {
-        this.dificuldade = dificuldade;
-    }*/
 }
