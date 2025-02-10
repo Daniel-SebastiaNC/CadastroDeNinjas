d6653a17a94af7dc5572065bd6a955f271e20f7e
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-09 21:13:00 -0300

    Chore: inclui o dto e os mapper para entidade ninja

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,36 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(ninjaModel.getId());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+
+        return ninjaDTO;
+    }
 }
